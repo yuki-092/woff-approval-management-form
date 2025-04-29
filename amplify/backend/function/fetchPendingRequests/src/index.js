a35e7fe64fdce1c96bf677622dd7067994515da7
yuki-092 <yuki.studio092@gmail.com>
2025-04-29 21:03:22 +0900

    Updated approval form with comments for approvers

--- amplify/backend/function/fetchPendingRequests/src/index.js
+++ amplify/backend/function/fetchPendingRequests/src/index.js
@@ -2,9 +2,10 @@ const AWS = require('aws-sdk');
 const dynamoDB = new AWS.DynamoDB.DocumentClient(); 
 
 exports.handler = async (event) => {
-  console.log('#### START fetch data')
-  const params = {
-    TableName: 'LeaveRequests', // DynamoDB のテーブル名
+  console.log('#### START fetch data');
+
+  const paramsLeaveRequests = {
+    TableName: 'LeaveRequests', // LeaveRequests table
     FilterExpression: 'approver1Status = :status1 OR approver2Status = :status2',
     ExpressionAttributeValues: {
       ':status1': '承認待ち',
@@ -12,23 +13,55 @@ exports.handler = async (event) => {
     }
   };
 
+  // const paramsRingiRequests = {
+  //   TableName: 'RingiRequests', // RingiRequests table
+  //   FilterExpression: 'approver1Status = :status1 OR approver2Status = :status2',
+  //   ExpressionAttributeValues: {
+  //     ':status1': '承認待ち',
+  //     ':status2': '承認待ち'
+  //   }
+  // };
+
   try {
-    const data = await dynamoDB.scan(params).promise();
-    console.log("SUCCESS: " + JSON.stringify({ items: data.Items }))
+    const [leaveData, ringiData] = await Promise.all([
+      dynamoDB.scan(paramsLeaveRequests).promise(),
+      // dynamoDB.scan(paramsRingiRequests).promise()
+    ]);
+
+    console.log("SUCCESS: " + JSON.stringify({
+      leaveRequests: {
+        type: '休暇申請',
+        items: leaveData.Items
+      },
+      // ringiRequests: {
+      //   type: '稟議書申請',
+      //   items: ringiData.Items
+      // }
+    }))
+
     return {
       statusCode: 200,
       headers: {
-        "Access-Control-Allow-Origin": "*", // CORS ヘッダーを追加
+        "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({ items: data.Items })
+      body: JSON.stringify({
+        leaveRequests: {
+          type: '休暇申請',
+          items: leaveData.Items
+        },
+        // ringiRequests: {
+        //   type: '稟議書申請',
+        //   items: ringiData.Items
+        // }
+      })
     };
   } catch (error) {
-    console.error('### ERROR:' + error.message)
+    console.error('### ERROR:' + error.message);
     return {
       statusCode: 500,
       headers: {
-        "Access-Control-Allow-Origin": "*", // CORS ヘッダーを追加
+        "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json"
       },
       body: JSON.stringify({ error: error.message })

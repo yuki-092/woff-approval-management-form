f730eab2a2fb25b9db9bed58bfb3ec8a8320dcb0
yuki-092 <yuki.studio092@gmail.com>
2025-09-06 13:35:40 +0900

    feat(route): Adminに個人情報変更一覧ページのルートを追加

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ApprovalManagementForm from './pages/ApprovalManagementForm';
 import AdminTopPage from './pages/admin/AdminTopPage';
 import LeavePage from './pages/admin/leave';
+import PersonalInfoPage from './pages/admin/personal-info';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<ApprovalManagementForm />} />
         <Route path="/admin" element={<AdminTopPage />} />
         <Route path="/admin/leave" element={<LeavePage />} />
+        <Route path="/admin/personal-info" element={<PersonalInfoPage />} />
       </Routes>
     </Router>
   );
